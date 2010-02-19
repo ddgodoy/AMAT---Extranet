049db976143eab802ac2ec9cd400d1c10733a254
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-02-19 16:30:36 +0000

    Mauro: Seguimos viendo temas raros en la intranet: 1. AL entrar en el formulario para editar una Circular, me muestra lo siguiente: (Ver imagenes adjuntas). 2. Intentamos asociar un Categoría Tema a la circular y No guarda los cambios

--- apps/frontend/modules/circulares/templates/editarSuccess.php
+++ apps/frontend/modules/circulares/templates/editarSuccess.php
@@ -2,11 +2,11 @@
 	<!--<strong><?php //echo __('Administraci&oacute;n') ?> </strong>&gt;-->
 	<strong>Administraci&oacute;n </strong>&gt; 
 	<a href="<?php echo url_for('iniciativas/index') ?>">Iniciativas</a> &gt; 
-	Actualizar Iniciativa
+	Actualizar Circular
 </div>
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
 	<tr>
-		<td width="95%"><h1>Actualizar Iniciativa</h1></td>
+		<td width="95%"><h1>Actualizar Circular</h1></td>
 		<td width="5%" align="right">
 			<a href="#">
 				<?php echo image_tag('pregunta.gif', array('alt' => 'Ayuda', 'id' => 'sprytrigger1', 'width' => '29', 'height' => '30', 'border' => '0')) ?>
