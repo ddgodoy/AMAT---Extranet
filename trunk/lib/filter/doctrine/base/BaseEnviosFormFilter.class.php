00d3a59deb9fe7b92f8cf786bfb0e5487d46f902
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-03-01 13:17:16 +0000

    Mauro: cambio de modelo para la gestión de error de envíos de comunicados

--- trunk/lib/filter/doctrine/base/BaseEnviosFormFilter.class.php
+++ trunk/lib/filter/doctrine/base/BaseEnviosFormFilter.class.php
@@ -19,7 +19,6 @@ class BaseEnviosFormFilter extends BaseFormFilterDoctrine
       'message_id' => new sfWidgetFormFilterInput(),
       'created_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => true)),
       'updated_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => true)),
-      'deleted'    => new sfWidgetFormChoice(array('choices' => array('' => 'yes or no', 1 => 'yes', 0 => 'no'))),
     ));
 
     $this->setValidators(array(
@@ -28,7 +27,6 @@ class BaseEnviosFormFilter extends BaseFormFilterDoctrine
       'message_id' => new sfValidatorPass(array('required' => false)),
       'created_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDate(array('required' => false)))),
       'updated_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDate(array('required' => false)))),
-      'deleted'    => new sfValidatorChoice(array('required' => false, 'choices' => array('', 1, 0))),
     ));
 
     $this->widgetSchema->setNameFormat('envios_filters[%s]');
@@ -52,7 +50,6 @@ class BaseEnviosFormFilter extends BaseFormFilterDoctrine
       'message_id' => 'Text',
       'created_at' => 'Date',
       'updated_at' => 'Date',
-      'deleted'    => 'Boolean',
     );
   }
 }
