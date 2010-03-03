cb2fb44a5e5f95285ed2eb275c7b7177029a539d
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-03-03 14:35:20 +0000

    Mauro: Arreglar error en el formulario de alta de organismos [IT'S DONE]

--- apps/frontend/modules/circulares/templates/_form.php
+++ apps/frontend/modules/circulares/templates/_form.php
@@ -78,7 +78,7 @@
 															options_for_select(array('0'=>'-- seleccionar --') + _get_options_from_objects($arrayCategoria),
 															$subcategoria_organismos_selected), array('style'=>'width:330px;','class'=>'form_input')
 														 );
-							echo observe_field('categoria_organismo_id', array('update'=>'content_sub_org','url'=>'subcategoria_organismos/listByCategoriaOrganismo','with'=>"'id_categoria_organismo='+value"));
+							echo observe_field('categoria_organismo_id', array('update'=>'content_sub_org','url'=>'subcategoria_organismos/listByCategoriaOrganismo','with'=>"'id_categoria_organismo='+value+'&name=circular'"));
 						?>
 						
 						<?php /*
--- lib/form/doctrine/CircularForm.class.php
+++ lib/form/doctrine/CircularForm.class.php
@@ -17,6 +17,8 @@ class CircularForm extends BaseCircularForm
       'contenido' => new fckFormWidget(),
       'numero'    => new sfWidgetFormInput(array(), array('style'=>'width:130px;','class'=>'form_input')),
       'documento' => new sfWidgetFormInputFileEditable(array('file_src' => 'uploads/circulares/docs', 'template'  => '<div><label></label>%input%<br /><label></label>%delete%<label> Eliminar documento actual</label></div>', ), array('class' => 'form_input')),
+      'circular_sub_tema_id'      => new sfWidgetFormDoctrineChoice(array('model' => 'CircularSubTema', 'add_empty' => true)),
+      'subcategoria_organismo_id' => new sfWidgetFormDoctrineChoice(array('model' => 'SubCategoriaOrganismo', 'add_empty' => true)),
     ));
 
     $this->setValidators(array(
@@ -26,8 +28,8 @@ class CircularForm extends BaseCircularForm
       'contenido' 			=> new sfValidatorString(array('required' => true), array('required'=>'El Contenido es obligatorio')),
       'numero'    			=> new sfValidatorString(array('max_length' => 100, 'required' => false) ),
       'documento'         	=> new sfValidatorFile(array('path' => 'uploads/circulares/docs', 'required' => false) ),
-      'circular_sub_tema_id' => new sfValidatorNumber(array('required' => false)),
-      'subcategoria_organismo_id' => new sfValidatorNumber(array('required' => false)),
+      'circular_sub_tema_id'      => new sfValidatorDoctrineChoice(array('model' => 'CircularSubTema', 'required' => false)),
+      'subcategoria_organismo_id' => new sfValidatorDoctrineChoice(array('model' => 'SubCategoriaOrganismo', 'required' => false)),
     ));
 
     $this->widgetSchema->setLabels(array(
