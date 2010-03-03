59b293bd5ef230bdab82d231ce752871876a8eb6
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-03-03 18:54:33 +0000

    Mauro: 4.4 Gestion de tématicas y organismos. El crear una categoría el campo titulo debe ser un input simple y no un textarea [IT'S DONE]

--- lib/form/doctrine/CategoriaOrganismoForm.class.php
+++ lib/form/doctrine/CategoriaOrganismoForm.class.php
@@ -11,5 +11,24 @@ class CategoriaOrganismoForm extends BaseCategoriaOrganismoForm
 {
   public function configure()
   {
+  	
+    $this->setWidgets(array(
+      'id'         => new sfWidgetFormInputHidden(),
+      'nombre'     => new sfWidgetFormInput(),
+      'created_at' => new sfWidgetFormDateTime(),
+      'updated_at' => new sfWidgetFormDateTime(),
+    ));
+
+    $this->setValidators(array(
+      'id'         => new sfValidatorDoctrineChoice(array('model' => 'CategoriaOrganismo', 'column' => 'id', 'required' => false)),
+      'nombre'     => new sfValidatorString(array('required' => false)),
+      'created_at' => new sfValidatorDateTime(array('required' => false)),
+      'updated_at' => new sfValidatorDateTime(array('required' => false)),
+    ));
+
+    $this->widgetSchema->setNameFormat('categoria_organismo[%s]');
+
+  	
+  	
   }
 }
