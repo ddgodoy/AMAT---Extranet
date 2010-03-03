9edf9f94e62df3ade27a4241197741863184fdfa
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-03-03 12:55:17 +0000

    Mauro: Error al eliminar Apps sistema [IT'S DONE]

--- trunk/lib/form/doctrine/CategoriaCTForm.class.php
+++ trunk/lib/form/doctrine/CategoriaCTForm.class.php
@@ -11,5 +11,25 @@ class CategoriaCTForm extends BaseCategoriaCTForm
 {
   public function configure()
   {
+  	$this->setWidgets(array(
+      'id'         => new sfWidgetFormInputHidden(),
+      'nombre'     => new sfWidgetFormInput(),
+      'created_at' => new sfWidgetFormDateTime(),
+      'updated_at' => new sfWidgetFormDateTime(),
+      'deleted'    => new sfWidgetFormInputCheckbox(),
+    ));
+
+    $this->setValidators(array(
+      'id'         => new sfValidatorDoctrineChoice(array('model' => 'CategoriaDG', 'column' => 'id', 'required' => false)),
+      'nombre'     => new sfValidatorString(array('required' => true),array('required' => 'Ingrese el Titulo')),
+      'created_at' => new sfValidatorDateTime(array('required' => false)),
+      'updated_at' => new sfValidatorDateTime(array('required' => false)),
+      'deleted'    => new sfValidatorBoolean(),
+    ));
+
+    $this->widgetSchema->setNameFormat('categoria_ct[%s]');
+  	
+  	
+  	
   }
 }
