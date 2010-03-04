5165ec0d470732f329d08ad8be14268b55f60d67
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-03-04 21:07:11 +0000

    Mauro:  3.5.1 Alta de ciruclares, la validación todos los campos son de caracter obligatorio

--- trunk/apps/frontend/modules/noticias/templates/indexSuccess.php
+++ trunk/apps/frontend/modules/noticias/templates/indexSuccess.php
@@ -101,7 +101,14 @@ use_helper('Text');?>
 		          <td width="11%" align="center"  style="border-bottom:1px dotted #999; padding:7px;"><div style="border-right:solid 1px #CCC"> <span style="color:#999"><?php	echo date("d/m/Y", strtotime($noticia->getFecha()));?></span><br />
 		              <?php		
 						if ($noticia->getImagen()) {
-							echo image_tag('/uploads/noticias/images/'.$noticia->getImagen(), array('height' => 40, 'width' => 80, 'alt' => $noticia->getTitulo()));
+							if(file_exists(sfConfig::get('sf_upload_dir').'/noticias/images/s_'.$noticia->getImagen()))
+							{
+								echo image_tag('/uploads/noticias/images/s_'.$noticia->getImagen(), array('alt' => $noticia->getTitulo()));
+							}
+							else 
+							{
+								echo image_tag('/uploads/noticias/images/'.$noticia->getImagen(), array('height' => 60, 'width' => 80, 'alt' => $noticia->getTitulo()));
+							}
 						}
 						else {	
 							echo image_tag('noimage.jpg', array('height' => 50, 'width' => 50, 'alt' => $noticia->getTitulo()));
