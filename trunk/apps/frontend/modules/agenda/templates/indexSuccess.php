38e5f4c890fc0dc7094bb34caab54de71a0e371e
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-03-08 18:08:18 +0000

    Mauro: En la Agenda falta el buscador. (Página 15) [IT'S DONE]

--- trunk/apps/frontend/modules/agenda/templates/indexSuccess.php
+++ trunk/apps/frontend/modules/agenda/templates/indexSuccess.php
@@ -1,6 +1,7 @@
 <?php use_helper('Fecha') ?>
 <?php use_helper('TestPager') ?>
-
+<link type="text/css" rel="stylesheet" href="/js/calendario/dhtml_calendar.css" media="screen"></link>
+<script language="javascript" type="text/javascript" src="/js/calendario/dhtml_calendar.js"></script>
 <div class="mapa">
 	<strong>Canal Corporativo</strong> &gt; Agenda de Eventos
 	<table width="100%" cellspacing="0" cellpadding="0" border="0">
@@ -59,6 +60,50 @@
 
 	<div class="rightside">
 		<div class="paneles"><?php include_component('agenda', 'agenda') ?></div>
+		<div class="paneles">
+				<h1>Buscar</h1>
+				<form method="post" enctype="multipart/form-data" action="<?php echo url_for('agenda/index') ?>">
+				<table width="100%" cellspacing="4" cellpadding="0" border="0">
+					<tbody>
+						<tr>
+							<td><label>Por T&iacute;tulo:</label></td>
+							<td><input type="text" onblur="this.style.background='#E1F3F7'" onfocus="this.style.background='#D5F7FF'" style="width:89%;" name="caja_busqueda" value="<?php echo $cajaBsq ?>" class="form_input"/></td>
+						</tr>
+						<tr>
+							<td width="29%"><label>Fecha Desde:</label></td>
+							<td width="71%" valign="middle">
+								<input type="text" onblur="this.style.background='#E1F3F7'" onfocus="this.style.background='#D5F7FF'" style="width:80px;" name="desde_busqueda" id="desde_busqueda" value="<?php echo $desdeBsq ?>" class="form_input"/>
+								<img border="0" style="margin-bottom: -3px;" src="/images/calendario.gif" class="clickeable" onclick="displayCalendar('desde_busqueda', this);"/>
+							</td>
+						</tr>
+						<tr>
+							<td style="padding-top: 5px;"><label>Fecha Hasta:</label></td>
+							<td style="padding-top: 5px;">
+								<input type="text" onblur="this.style.background='#E1F3F7'" onfocus="this.style.background='#D5F7FF'" style="width:80px;" name="hasta_busqueda" id="hasta_busqueda" value="<?php echo $hastaBsq ?>" class="form_input"/>
+								<img border="0" style="margin-bottom: -3px;" src="/images/calendario.gif" class="clickeable" onclick="displayCalendar('hasta_busqueda', this);"/>
+							</td>
+						</tr>
+						<tr>
+							<td style="padding-top: 5px;"><label>Tipo</label></td>
+							<td style="padding-top: 5px;">
+							<?php echo select_tag('estado',options_for_select( array('0'=>'--seleccionar--',1 => 'Evento', '2' => 'Convocatoria'),$estadoBq),array('class'=>"form_input"))?>	
+						</tr>
+						<tr>
+						<td style="padding-top:5px;">
+									<span class="botonera"><input type="submit" class="boton" value="Buscar" name="btn_buscar"/></span>	
+								</td>
+								<td>
+									<?php if ($cajaBsq  || $desdeBsq || $hastaBsq || $estadoBq ): ?>
+									<span class="botonera"><input type="submit" class="boton" value="Limpiar" name="btn_quitar"/></span>
+									<?php endif; ?>								
+								
+								</td>	
+						</tr>
+					</tbody>
+				</table>
+				</form>
+			</div>
+		</div>
 	</div>
 	<br clear="all" />
 </div>
