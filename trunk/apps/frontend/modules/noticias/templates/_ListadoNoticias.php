dd28819a7b2330eb41eb8916303ed8f32dec1bd0
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-03-11 17:15:37 +0000

    Mauro: En el listado de noticias del administrador subir la fecha encima de la imagen [IT'S DONE]

--- trunk/apps/frontend/modules/noticias/templates/_ListadoNoticias.php
+++ trunk/apps/frontend/modules/noticias/templates/_ListadoNoticias.php
@@ -2,28 +2,23 @@
 				<?php if (validate_action('baja')): ?>
 				<td><input type="checkbox" name="id[]" value="<?php echo $noticia->getId() ?>" /></td>
 				<?php endif; ?>
-		        <td>
-				<?php
-				echo date("d/m/Y", strtotime($noticia->getFecha()));
-				?>
 			    </td>
-				<td align="center">
+				<td  width="11%" align="center"><span style="color:#999"><?php	echo date("d/m/Y", strtotime($noticia->getFecha()));?></span><br />
 				<?php		
-				if ($noticia->getImagen()) 
-				{
-					if(file_exists(sfConfig::get('sf_upload_dir').'/noticias/images/s_'.$noticia->getImagen()))
-					{
-						echo image_tag('/uploads/noticias/images/s_'.$noticia->getImagen(), array('alt' => $noticia->getTitulo()));
+					if ($noticia->getImagen()) {
+						if(file_exists(sfConfig::get('sf_upload_dir').'/noticias/images/s_'.$noticia->getImagen()))
+						{
+							echo image_tag('/uploads/noticias/images/s_'.$noticia->getImagen(), array('alt' => $noticia->getTitulo()));
+						}
+						else 
+						{
+							echo image_tag('/uploads/noticias/images/'.$noticia->getImagen(), array('height' => 60, 'width' => 80, 'alt' => $noticia->getTitulo()));
+						}
 					}
-					else 
-					{
-						echo image_tag('/uploads/noticias/images/'.$noticia->getImagen(), array('height' => 40, 'width' => 80, 'alt' => $noticia->getTitulo()));
+					else {	
+						echo image_tag('noimage.jpg', array('height' => 50, 'width' => 50, 'alt' => $noticia->getTitulo()));
 					}
-				}
-				else {	
-					echo image_tag('noimage.jpg', array('height' => 50, 'width' => 50, 'alt' => $noticia->getTitulo()));
-				}
-				?>
+				 ?>
 				</td>
 				<td >
 				<br clear="all">
