69c16c2f08e4c498751b7793862fafc5671f3423
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-03-11 18:04:38 +0000

    Mauro: En la home las imagenes de las noticias destacadas se estan deformando. [IT'S DONE]

--- trunk/apps/frontend/modules/usuarios/actions/actions.class.php
+++ trunk/apps/frontend/modules/usuarios/actions/actions.class.php
@@ -24,8 +24,7 @@ class usuariosActions extends sfActions
 	    ->leftJoin('u.UsuarioGrupoTrabajo ug') 
 	    ->leftJoin('u.UsuarioConsejoTerritorial uc') 
 	    ->leftJoin('u.UsuarioRol ur') 
-		->where('u.id>1')
-		->andWhere($this->setFiltroBusqueda())
+		->Where($this->setFiltroBusqueda())
 		->orderBy($this->setOrdenamiento());
 		
 		
