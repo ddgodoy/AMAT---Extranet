8aecab5af7cfa67582c7c88952ae8001aafb0691
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-03-15 14:50:35 +0000

    Mauro: Al crear un nuevo Perfil, el campo nombre debe ser un campo de texto y no un textarea [IT'S DONE]

--- lib/form/doctrine/RolForm.class.php
+++ lib/form/doctrine/RolForm.class.php
@@ -11,5 +11,28 @@ class RolForm extends BaseRolForm
 {
   public function configure()
   {
+  	$this->setWidgets(array(
+      'id'            => new sfWidgetFormInputHidden(),
+      'nombre'        => new sfWidgetFormInput(array(), array('class' => 'form_input', 'style' => 'width:280px;')),
+      'detalle'       => new sfWidgetFormTextarea(array(), array('rows' => '5', 'style' => 'width:280px;')),
+      'codigo'        => new sfWidgetFormInput(array(), array('class' => 'form_input', 'style' => 'width:280px;')),
+      'created_at'    => new sfWidgetFormDateTime(),
+      'updated_at'    => new sfWidgetFormDateTime(),
+      'deleted'       => new sfWidgetFormInputCheckbox(),
+      'usuarios_list' => new sfWidgetFormDoctrineChoiceMany(array('model' => 'Usuario')),
+    ));
+
+    $this->setValidators(array(
+      'id'            => new sfValidatorDoctrineChoice(array('model' => 'Rol', 'column' => 'id', 'required' => false)),
+      'nombre'        => new sfValidatorString(array('required' => false)),
+      'detalle'       => new sfValidatorString(array('required' => false)),
+      'codigo'        => new sfValidatorString(array('max_length' => 32)),
+      'created_at'    => new sfValidatorDateTime(array('required' => false)),
+      'updated_at'    => new sfValidatorDateTime(array('required' => false)),
+      'deleted'       => new sfValidatorBoolean(),
+      'usuarios_list' => new sfValidatorDoctrineChoiceMany(array('model' => 'Usuario', 'required' => false)),
+    ));
+
+    $this->widgetSchema->setNameFormat('rol[%s]');
   }
 }
