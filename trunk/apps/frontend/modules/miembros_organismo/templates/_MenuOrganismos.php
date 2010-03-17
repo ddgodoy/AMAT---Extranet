b0ed26528372ec929cbd29864b54e7142641316d
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-03-17 14:21:21 +0000

    Mauro: Entrando con perfil de Responsable de trabajo en grupos de trabajo > Miembros aparece la opción Normas de funcionamiento que al hacer click sobre ella indica que no tenemos permisos para acceder [IT'S DONE]

--- trunk/apps/frontend/modules/miembros_organismo/templates/_MenuOrganismos.php
+++ trunk/apps/frontend/modules/miembros_organismo/templates/_MenuOrganismos.php
@@ -1,8 +1,8 @@
 <h2 class="grupo"><?php if ($Organismos): echo $Organismos->getNombre(); $get = '?organismo='.$Organismos->getId(); else: $get= ''; echo 'Organismos'; endif; ?> </h2>
        <div class="navegacion-grupos">
        <!--<a <?php //if($modulo != 'normas_de_funcionamientos'):?> href="<?php //echo url_for('normas_de_funcionamientos/index'.$get) ?>" <?php // else: ?> class="seleccionado" <?php //endif; ?>>Normas de Funcionamiento</a> | -->
-       <a <?php if($modulo != 'miembros_organismo'):?> href="<?php echo url_for('miembros_organismo/index'.$get) ?>" <?php else: ?> class="seleccionado" <?php endif; ?>>Miembros del Grupo de Trabajo</a> | 
-       <a <?php if($modulo != 'asambleas'):?> href="<?php echo url_for('asamblea_organismo/index'.$get) ?>" <?php else: ?> class="seleccionado" <?php endif; ?>>Convocatorias</a> | 
-       <a <?php if($modulo != 'acta'):?> href="<?php echo url_for('acta_organismos/index'.$get) ?>" <?php else: ?> class="seleccionado" <?php endif; ?>>Actas </a> | 
-       <a <?php if($modulo != 'documentacion_organismos'):?> href="<?php echo url_for('documentacion_organismos/index'.$get) ?>" <?php else:?> class="seleccionado" <?php endif; ?>>Documentaci&oacute;n</a>
+       <?php if ( validate_action('listar','miembros_organismo')): ?><a <?php if($modulo != 'miembros_organismo'):?> href="<?php echo url_for('miembros_organismo/index'.$get) ?>" <?php else: ?> class="seleccionado" <?php endif; ?>>Miembros del Grupo de Trabajo</a> | <?php endif;?>
+       <?php if ( validate_action('listar','asambleas')): ?><a <?php if($modulo != 'asambleas'):?> href="<?php echo url_for('asamblea_organismo/index'.$get) ?>" <?php else: ?> class="seleccionado" <?php endif; ?>>Convocatorias</a> | <?php endif;?>
+       <?php if ( validate_action('listar','acta')): ?><a <?php if($modulo != 'acta'):?> href="<?php echo url_for('acta_organismos/index'.$get) ?>" <?php else: ?> class="seleccionado" <?php endif; ?>>Actas </a> | <?php endif;?>
+       <?php if ( validate_action('listar','documentacion_organismos')): ?><a <?php if($modulo != 'documentacion_organismos'):?> href="<?php echo url_for('documentacion_organismos/index'.$get) ?>" <?php else:?> class="seleccionado" <?php endif; ?>>Documentaci&oacute;n</a><?php endif;?>
 </div>
