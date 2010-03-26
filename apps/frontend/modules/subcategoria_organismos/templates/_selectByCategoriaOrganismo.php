595f1bd50f02ecfbe08bb3b345566ef9cffa4a92
elio <elio@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-03-26 18:12:22 +0000

    ELIO: ordenamiento organismos por nombre asc

--- apps/frontend/modules/subcategoria_organismos/templates/_selectByCategoriaOrganismo.php
+++ apps/frontend/modules/subcategoria_organismos/templates/_selectByCategoriaOrganismo.php
@@ -1,5 +1,6 @@
 <?php
 	use_helper('Object');
+
 	echo select_tag($name.'[subcategoria_organismo_id]',
 	options_for_select( array('0'=>'-- seleccionar --') + _get_options_from_objects($arraySubcategoria), $subcategoria_organismos_selected),
 	array('style'=>'width:250px;','class'=>'form_input'));
