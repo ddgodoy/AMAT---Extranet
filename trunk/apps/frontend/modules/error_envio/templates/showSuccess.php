7ec75bfc200999204ec9c45cfe70a51d31422460
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-03-29 19:55:55 +0000

    Mauro: En el listado de errores de envio, que cuando se haga clic se vea el error completo [it's done]

--- trunk/apps/frontend/modules/error_envio/templates/showSuccess.php
+++ trunk/apps/frontend/modules/error_envio/templates/showSuccess.php
@@ -17,12 +17,9 @@
 	  <a class="notentrada">Email: <a class="notentrada" href="<?php echo url_for('usuarios/editar?id='.$error_envio->getUsuarioId()) ?>"><?php echo $error_envio->Usuario->getEmail() ?></a></a><br /><br />
 	  <div class="ul_noticias"><?php echo nl2br($error_envio->getError()) ?></div> 
 	  <br clear="all" /> 
-	  <br><br>
 	   <div class="clear"></div>
 	</div>
-	   
 	<br clear="all" />
-	
 	<div class="botonera"> 
 	  <input type="button" id="boton_cancel" class="boton" value="Volver" name="boton_cancel" onclick="document.location='<?php echo url_for('error_envio/index') ?>';"/>
 	</div>
