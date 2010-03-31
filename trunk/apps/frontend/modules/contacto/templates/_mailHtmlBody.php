3139d0636a67b6878ee33f65c95155018b326089
elio <elio@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-03-31 19:48:34 +0000

    ELIO: cambio email contacto

--- trunk/apps/frontend/modules/contacto/templates/_mailHtmlBody.php
+++ trunk/apps/frontend/modules/contacto/templates/_mailHtmlBody.php
@@ -1,14 +1,51 @@
 <html>
 	<head>
-	<title>Contacto</title>
+	<title><?php echo $tema ?></title>
+	<style type="text/css">
+		<!--
+		table {
+			font-family: Verdana, Geneva, sans-serif;
+			color: #333;
+			font-size: 12px;
+		}
+		body table tr td {
+			border-top-width: 1px;
+			border-top-style: dotted;
+			border-top-color: #E1E1E1;
+			padding: 5px;
+		}
+		body table tr th h1 {
+			font-size: 16px;
+			line-height: 30px;
+			color: #006699;
+			margin-left: 5px;
+		}
+		.head {
+			background-image: url(<?php echo $head_image ?>);
+			height: 76px;
+			width: 100%;
+			margin-bottom: 5px;
+		}
+		-->
+	</style>
 	</head>
 	<body>
+		<div class="head"></div>
 		<table width="100%" border="0" cellpadding="0" cellspacing="5">
 			<tr>
-				<td><img src="<?php echo $head_image ?>" /></td>
+				<th colspan="2" align="left"><h1><?php echo $tema;?></h1></th>
 			</tr>
 			<tr>
-				<th colspan="2" align="left"><h1>Contacto_00</h1></th>
+				<td width="7%"><strong>Usuario:</strong>&nbsp;</td>
+				<td width="93%"><?php echo $usuario ?></td>
+			</tr>
+			<tr>
+				<td><strong>Organiza:</strong>&nbsp;</td>
+				<td><em><?php echo $organizacion ?></em></td>
+			</tr>
+			<tr>
+				<td valign="top"><strong>Asunto:</strong>&nbsp;</td>
+				<td><?php echo nl2br($asunto) ?></td>
 			</tr>
 		</table>
 	</body>
