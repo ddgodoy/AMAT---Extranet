32d0f6e262fc6e751d1a52899d2785764be59ccf
elio <elio@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-03-31 19:20:42 +0000

    ELIO: prueba 02 email contacto

--- trunk/apps/frontend/modules/contacto/templates/_mailHtmlBody.php
+++ trunk/apps/frontend/modules/contacto/templates/_mailHtmlBody.php
@@ -5,6 +5,9 @@
 	<body>
 		<table width="100%" border="0" cellpadding="0" cellspacing="5">
 			<tr>
+				<td><img src="<?php echo $head_image ?>" /></td>
+			</tr>
+			<tr>
 				<th colspan="2" align="left"><h1>Contacto</h1></th>
 			</tr>
 		</table>
