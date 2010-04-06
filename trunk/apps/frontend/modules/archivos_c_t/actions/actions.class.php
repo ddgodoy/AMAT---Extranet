c789311cfe8151657d0a3842744b621132148f5e
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-04-06 12:47:02 +0000

    Mauro:  [CZC-183864] Error en Listado [IT'S DONE]

--- trunk/apps/frontend/modules/archivos_c_t/actions/actions.class.php
+++ trunk/apps/frontend/modules/archivos_c_t/actions/actions.class.php
@@ -209,8 +209,8 @@ class archivos_c_tActions extends sfActions
   
   protected function setOrdenamiento()
   {
-		$this->orderBy = 'nombre';
-		$this->sortType = 'asc';
+		$this->orderBy = 'fecha';
+		$this->sortType = 'desc';
 
 		if ($this->hasRequestParameter('orden')) {
 			$this->orderBy = $this->getRequestParameter('sort');
