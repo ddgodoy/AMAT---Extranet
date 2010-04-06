b14d8f75f0128104c9a56d9cbd13483729a89d06
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-04-06 16:41:46 +0000

    Mauro:   problema inicio

--- trunk/lib/model/doctrine/NotificacionTable.class.php
+++ trunk/lib/model/doctrine/NotificacionTable.class.php
@@ -38,8 +38,6 @@ class NotificacionTable extends Doctrine_Table
 		$q->groupBy('entidad_id');
 		if($limit) $q->limit($limit);
 
-                echo $q->getQuery();
-                exit ();
 
 		$notificaciones = $q->execute();
 		
