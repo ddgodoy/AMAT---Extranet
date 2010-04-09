caa1dc820d2079ba173fd2141aabcf9e5e9db9c0
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-04-09 16:52:13 +0000

    Mauro:   Error en mapa web [IT'S DONE]

--- apps/frontend/modules/mapasitio/templates/indexSuccess.php
+++ apps/frontend/modules/mapasitio/templates/indexSuccess.php
@@ -3,7 +3,14 @@
     <div class="mapaweb">
       <h1>Mapa de Web</h1>
  <?php $r = 0; $arrMenu = sfContext::getInstance()->getUser()->getAttribute('menu') ?>
-      <?php foreach ($arrMenu as $item) : $r++;?>
+  <?php foreach ($arrMenu as $item) : $r++; $banderaInicio = 0; ?>
+    <?php if (count($item['hijos']) >=1) :?>
+        <?php foreach ($item['hijos'] as $verelmoduloInicial) : ?>
+             <?php if(validate_action('listar',$verelmoduloInicial['modulo'])|| validate_action('publicar',$verelmoduloInicial['modulo']) ):
+                $banderaInicio = 1;
+              endif;?>
+             <?php endforeach; ?>
+       <?php if($banderaInicio == 1): ?>
        <?php if($r == 1 ){ echo '<div class="col1">';}?>
          <ul class="tema"> 
            <li> 
@@ -30,19 +37,21 @@
 	        <?php endif; ?>	
 	       </li>
 	      </ul>
-	     <br clear="all" />     
-	    <?php if($r == 2 ){ echo '</div>'; $r = 0;}?> 
+	     <br clear="all" />
+	    <?php if($r == 2 ){ echo '</div>'; $r = 0;}?>
+           <?php endif; ?>
+        <?php endif; ?>
       <?php endforeach; ?> 
        <br clear="all" />
       <br clear="all" />
-      <ul class="especial">
+      <br clear="all" />
+     </div>
+    <br clear="all" />
+    <ul class="especial">
         <h4>Otros Links</h4>
-       <li></li> <a href="<?php echo url_for('inicio/index') ?>" class="a">INICIO</a></li>
+       <li><a href="<?php echo url_for('inicio/index') ?>" class="a">INICIO</a></li>
 		<li><a href="<?php echo url_for('mapasitio/index') ?>" class="d">MAPA DE WEB</a></li>
 		<li><a href="<?php echo url_for('usuarios/perfil') ?>" class="b">MIS DATOS</a></li>
         <li><a href="<?php echo url_for('contacto/index') ?>">CONTACTO</a></li>
       </ul> 
-      <br clear="all" />       
-     </div>
-    <br clear="all" /> 
 </div>  
