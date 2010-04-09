3d16c175f4fe3b695647634f8f74dfc346d02cc4
jacobo <jacobo@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-04-09 09:53:15 +0000

    error 404 customizado

--- apps/frontend/modules/default/templates/error404Success.php
+++ apps/frontend/modules/default/templates/error404Success.php
@@ -1,5 +1,5 @@
 <h2>Error 404: Archivo no encontrado.</h2><br />
 <h3>La url a la que quiere acceder no existe.</h3><br />
 <ul>
-	<li><a href="<?php echo url_for('inicio/index') ?>">Volver a la página principal</a></li>
+	<li><a href="<?php echo url_for('seguridad/index') ?>">Volver a la página principal</a></li>
 </ul>
