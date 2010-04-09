50fc7993d34d5b3310c290a7726fb46526526efd
jacobo <jacobo@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-04-09 11:00:50 +0000

    error 500 custom

---
+++ apps/frontend/modules/seguridad/templates/errorSuccess.php
@@ -0,0 +1,5 @@
+<h2>Se ha producido un error.</h2><br />
+<h3>Estamos trabajando para solucionarlo, disculpe las molestias.</h3><br />
+<ul>
+	<li><a href="<?php echo url_for('seguridad/index') ?>">Volver a la página principal</a></li>
+</ul>
