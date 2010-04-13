ea4384dd3fcd887f69454ca4404691fd7156dcf5
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-04-13 13:33:45 +0000

    Mauro:   Error con "error 404" [IT'S DONE]

--- trunk/apps/frontend/modules/seguridad/templates/errorSuccess.php
+++ trunk/apps/frontend/modules/seguridad/templates/errorSuccess.php
@@ -1,5 +1,18 @@
-<h2>Se ha producido un error.</h2><br />
-<h3>Estamos trabajando para solucionarlo, disculpe las molestias.</h3><br />
-<ul>
-	<li><a href="<?php echo url_for('seguridad/index') ?>">Volver a la página principal</a></li>
-</ul>
+<div class="mapa">
+	<strong>Administrador </strong>&gt;Error
+	</div>
+	<table width="100%" border="0" cellspacing="0" cellpadding="0">
+	  <tr>
+	    <td width="50%"><h1>Se ha producido un error.</h1></td>
+	    <td width="50%" align="right">&nbsp;</td>
+	  </tr>
+	</table>
+	<br clear="all" />
+	<br clear="all" />
+<div class="noticias">
+          <a class="nottit">Se ha producido un error.</a><br />
+          <p class="notentrada" align="justify">Estamos trabajando para solucionarlo, disculpe las molestias.</p>
+          <br clear="all" />
+	  <a href="<?php echo url_for('seguridad/index') ?>" class="notfecha">&gt;&gt; Volver a la página principal</a>
+	  <br clear="all" />
+</div>
