0c88b9e324e753b131c93bc6f15b951aa1a015ae
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-04-13 18:07:01 +0000

    Mauro:   Error en email al publicar noticia

--- apps/frontend/modules/noticias/templates/_ListadoNoticiaUsuarios.php
+++ apps/frontend/modules/noticias/templates/_ListadoNoticiaUsuarios.php
@@ -19,12 +19,12 @@
 </td>
 <td width="88%" valign="top" colspan="3"  style="border-bottom:1px dotted #999; padding:7px;"><a href="<?php echo url_for('noticias/show?id=' . $noticia->getId()) ?>"><strong><?php echo $noticia->getTitulo() ?></strong></a> <br clear="all">
 <?php if ($noticia->getEntradilla()): echo nl2br($noticia->getEntradilla()); endif; ?></td>
-<td valign="center" align="center">
+<td valign="center" align="center" style="border-bottom:1px dotted #999; padding:7px;">
 <?php if (validate_action('modificar') || $creador == 1):?>
 <a href="<?php echo url_for('noticias/editar?id='.$noticia->getId()) ?>"><?php echo image_tag('show.png', array('height' => 20, 'width' => 17, 'border' => 0, 'title' => 'Ver')) ?></a>
 <?php endif;?>
 </td>
-<td valign="center" align="center">
+<td valign="center" align="center" style="border-bottom:1px dotted #999; padding:7px;">
 <?php if(validate_action('baja') || $creador == 1):?>
 <?php echo link_to(image_tag('borrar.png', array('title' => 'Borrar', 'alt' => 'Borrar', 'width' => '20', 'height' => '20', 'border' => '0')), 'noticias/delete?id='.$noticia->getId(), array('method' => 'delete', 'confirm' => 'Est&aacute;s seguro que deseas eliminar la noticia ' . $noticia->getTitulo() . '?')) ?>
 <?php  endif;  ?>
