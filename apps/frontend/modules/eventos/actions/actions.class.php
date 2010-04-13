032b07bb1df07ebf7136a329ecd906058c20279f
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-04-13 12:32:23 +0000

    Mauro:   Error en envio de email guardando noticias

--- apps/frontend/modules/eventos/actions/actions.class.php
+++ apps/frontend/modules/eventos/actions/actions.class.php
@@ -126,7 +126,7 @@ class eventosActions extends sfActions
                                                             }
 							if ($emailPublic->getEmail() && preg_match('#^(((([a-z\d][\.\-\+_]?)*)[a-z0-9])+)\@(((([a-z\d][\.\-_]?){0,62})[a-z\d])+)\.([a-z\d]{2,6})$#i', $emailPublic->getEmail())) {
 								$mailer = new Swift(new Swift_Connection_NativeMail());
-								$message = new Swift_Message('Contacto desde Extranet de Asociados AMAT');
+								$message = new Swift_Message('Contacto desde Extranet Sectorial AMAT');
 
 								$mailContext = array('tema'   => 'Evento publicado',
 										 'evento' => $nombreEvento,
@@ -216,7 +216,7 @@ class eventosActions extends sfActions
 					$iPh = image_path('/images/logo_email.jpg', true);
           
                                       $mailer  = new Swift(new Swift_Connection_NativeMail());
-                                      $message = new Swift_Message('Contacto desde Extranet de Asociados AMAT');
+                                      $message = new Swift_Message('Contacto desde Extranet Sectorial AMAT');
 
                                       $mailContext = array('tema'   => $tema,
                                                            'evento' => $estado['titulo'],
