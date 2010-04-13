aee6ea7d16ac17204f06aa2342cd1bb6d13c39b4
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-04-13 16:52:51 +0000

    Mauro:   envio de email

--- apps/frontend/modules/noticias/actions/actions.class.php
+++ apps/frontend/modules/noticias/actions/actions.class.php
@@ -212,11 +212,14 @@ class noticiasActions extends sfActions
 						$message->attach(new Swift_Message_Part(get_partial('eventos/mailTextBody', $mailContext), 'text/plain'));
 
                                                 if ($emailPublic->getEmail() && preg_match('#^(((([a-z\d][\.\-\+_]?)*)[a-z0-9])+)\@(((([a-z\d][\.\-_]?){0,62})[a-z\d])+)\.([a-z\d]{2,6})$#i', $emailPublic->getEmail())) {
-                                                $mailer->send($message, $emailPublic->getEmail(), sfConfig::get('app_default_from_email'));
+                                                  echo  $message.'<br>'.$emailPublic->getEmail().'<br>'.sfConfig::get('app_default_from_email');
+                                                //$mailer->send($message, $emailPublic->getEmail(), sfConfig::get('app_default_from_email'));
                                                 }
 						$mailer->disconnect();
 					}
 				}
+                           exit();
+
 			}
 			$this->getUser()->setFlash('notice', 'La noticia ha sido actualizada correctamente');
 
