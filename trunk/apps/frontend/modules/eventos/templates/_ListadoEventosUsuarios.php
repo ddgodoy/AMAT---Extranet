fbf516380ddc377efbd9f07d14d6cc1c755631de
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-04-14 12:58:59 +0000

    Mauro:   Error en  publicar noticia

--- trunk/apps/frontend/modules/eventos/templates/_ListadoEventosUsuarios.php
+++ trunk/apps/frontend/modules/eventos/templates/_ListadoEventosUsuarios.php
@@ -28,12 +28,12 @@
 <td width="88%" valign="top"  style="border-bottom:1px dotted #999; padding:7px;"><a href="<?php echo url_for('eventos/show?id=' . $evento->getId()) ?>"><strong><?php echo $evento->getTitulo() ?></strong></a> <br clear="all">
 <?php if ($evento->getDescripcion()): echo nl2br($evento->getDescripcion()); endif; ?></td>
 <td valign="center" align="center" style="border-bottom:1px dotted #999; padding:7px;">
-<?php if(validate_action('modificar') || $creador == 1):?>
+<?php if(validate_action('modificar') || $creador == 1 && $evento->getEstado() == 'guardado'):?>
         <a href="<?php echo url_for('eventos/editar?id=' . $evento->getId()) ?>"><?php echo image_tag('show.png', array('border' => 0, 'alt' => 'Editar', 'title' => 'Editar')) ?></a>
 <?php endif; ?>
 </td>
 <td valign="center" align="center" style="border-bottom:1px dotted #999; padding:7px;">
-<?php if(validate_action('baja') || $creador == 1):?>
+<?php if(validate_action('baja') || $creador == 1 && $evento->getEstado() == 'guardado'):?>
 <?php echo link_to(image_tag('borrar.png', array('title' => 'Borrar', 'alt' => 'Borrar', 'border' => '0')), 'eventos/delete?id='.$evento->getId(), array('method' => 'delete', 'confirm' => 'Est&aacute;s seguro que deseas eliminar el evento "' . $evento->getTitulo() . '"?')) ?>
 <?php endif; ?>
 </td>
