d75d5890fd6d9c63d53de1bb7b4f310568d0e26f
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-04-20 17:05:59 +0000

    Mauro: Error con los emails

--- apps/frontend/modules/eventos/actions/actions.class.php
+++ apps/frontend/modules/eventos/actions/actions.class.php
@@ -121,8 +121,8 @@ class eventosActions extends sfActions
 						$organizador  = $evento->getOrganizador();
 						$descripcion  = $evento->getDescripcion();
 
-                                                $coneccion = new Swift_Connection_SMTP('217.116.20.132', 25, Swift_Connection_SMTP::ENC_SSL);
-                                                $coneccion->setUsername('alertas@extranet.amat.es');
+                                                $coneccion = new Swift_Connection_SMTP('217.116.20.132', 465);
+                                                $coneccion->setUsername('alertas.extranet.amat.es');
                                                 $coneccion->setPassword('4oddF=dohm(F');
 
 
