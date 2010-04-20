2de5dc4025d7a293cb7bef5cda0748fdde588cde
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-04-20 18:50:17 +0000

    Mauro:  Error con "error 404"

--- trunk/apps/frontend/modules/seguridad/actions/actions.class.php
+++ trunk/apps/frontend/modules/seguridad/actions/actions.class.php
@@ -217,7 +217,7 @@ class seguridadActions extends sfActions
               if (sfConfig::get('sf_logging_enabled'))
                 {
                   $url = "https://". $_SERVER['SERVER_NAME'] . "/". $_SERVER['REQUEST_URI'];
-                  $mensaje = date("F j, Y, g:i a").'symfony [err] {sfError404Exception} Empty module and/or action after parsing the URL "'.$url.'" (/).';
+                  $mensaje = date("F j, Y, g:i a").' - symfony [err] {sfError404Exception} Empty module and/or action after parsing the URL "'.$url.'" (/).';
                   sfContext::getInstance()->getLogger()->info($mensaje);
                 }
                 $this->setLayout("layout");
