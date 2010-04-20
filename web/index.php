d3779e4e0b7dbb6bfac9b0a55969ac433b0ccd6c
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-04-20 19:29:53 +0000

    Mauro:  Error con "error 404"

--- web/index.php
+++ web/index.php
@@ -3,7 +3,7 @@
   
   require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-	$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', true);
+	$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
   //$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', true);
 
 	sfContext::createInstance($configuration)->dispatch();
