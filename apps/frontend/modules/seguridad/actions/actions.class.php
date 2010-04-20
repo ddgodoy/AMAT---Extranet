2f86de89e434a7b9e3565c7df60ba6bce04107c1
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-04-20 19:05:24 +0000

    Mauro:  Error con "error 404"

--- apps/frontend/modules/seguridad/actions/actions.class.php
+++ apps/frontend/modules/seguridad/actions/actions.class.php
@@ -217,7 +217,7 @@ class seguridadActions extends sfActions
               if (sfConfig::get('sf_logging_enabled'))
                 {
                   $url = "https://". $_SERVER['SERVER_NAME'] . "/". $_SERVER['REQUEST_URI'];
-                  $mensaje = date("F j, Y, g:i a").' - {sfError404Exception} Empty module and/or action after parsing the URL "'.$url.'" (/).';
+                  $mensaje = date("F j, Y, g:i a").' - {sfError404Exception} Action "'.$url.'" does not exist.';
                   $this->logMessage($mensaje, 'err');
                   //sfContext::getInstance()->getLogger()->info($mensaje);
                 }
