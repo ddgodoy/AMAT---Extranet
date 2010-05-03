b12a5d1102f63555d5a0c3b7c04188eb2dfa7002
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-05-03 18:15:57 +0000

    Mauro: ERROR En GT he creado una nueva documentación y le ha llegado notificación a los usuarios que tiene perfil de miembro GT y que no estan activos

--- trunk/lib/form/doctrine/CategoriaNormativaForm.class.php
+++ trunk/lib/form/doctrine/CategoriaNormativaForm.class.php
@@ -11,5 +11,25 @@ class CategoriaNormativaForm extends BaseCategoriaNormativaForm
 {
   public function configure()
   {
+      $this->setWidgets(array(
+      'id'         => new sfWidgetFormInputHidden(),
+      'nombre'     => new sfWidgetFormInput(),
+      'contenido'  => new sfWidgetFormTextarea(),
+      'created_at' => new sfWidgetFormDateTime(),
+      'updated_at' => new sfWidgetFormDateTime(),
+      'deleted'    => new sfWidgetFormInputCheckbox(),
+    ));
+
+    $this->setValidators(array(
+      'id'         => new sfValidatorDoctrineChoice(array('model' => 'CategoriaNormativa', 'column' => 'id', 'required' => false)),
+      'nombre'     => new sfValidatorString(array('required' => false)),
+      'contenido'  => new sfValidatorString(array('required' => false)),
+      'created_at' => new sfValidatorDateTime(array('required' => false)),
+      'updated_at' => new sfValidatorDateTime(array('required' => false)),
+      'deleted'    => new sfValidatorBoolean(),
+    ));
+
+    $this->widgetSchema->setNameFormat('categoria_normativa[%s]');
+
   }
 }
