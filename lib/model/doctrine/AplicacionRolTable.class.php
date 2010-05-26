4fb3c8cb6e190dae070a01292c4857cd012e2ebb
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-05-26 15:59:05 +0000

    Mauro: Error en documentacion en GT

--- lib/model/doctrine/AplicacionRolTable.class.php
+++ lib/model/doctrine/AplicacionRolTable.class.php
@@ -56,6 +56,49 @@ class AplicacionRolTable extends Doctrine_Table
    	  return $r->execute();   
    }
 
+    public static function getEmailPublicarArray($IDaplic = '', $gru = '', $cons= '', $org = '')
+   {
+   	 $r=Doctrine_Query::create()
+         ->select('u.id AS id, u.email AS email')
+   	 ->from('Usuario u');
+   	 if($gru !='')
+   	 {
+   	    $r->leftJoin('u.UsuarioGrupoTrabajo ugr');
+   	 }
+   	 if($cons !='')
+   	 {
+   	   $r->leftJoin('u.UsuarioConsejoTerritorial uct');
+   	 }
+   	 if($org !='')
+   	 {
+   	   $r->leftJoin('u.UsuarioOrganismo uo');
+   	 }
+   	   $r->leftJoin('u.UsuarioRol ur')
+   	    ->leftJoin('ur.Rol r')
+   	    ->leftJoin('r.AplicacionRol ar')
+   	    ->where('ar.accion_publicar = 1');
+   	    if($IDaplic!='')
+   	    {
+   	     	$r->andWhere('ar.aplicacion_id = '.$IDaplic);
+   	    }
+   	    if($gru !='')
+	   	 {
+	   	    $r->andWhere('ugr.grupo_trabajo_id = '.$gru);
+	   	 }
+	   	 if($cons !='')
+	   	 {
+	   	   $r->andWhere('uct.consejo_territorial_id = '.$cons);
+	   	 }
+	   	 if($org !='')
+	   	 {
+	   	   $r->andWhere('uo.organismo_id = '.$org);
+	   	 }
+   	    $r->andWhere('u.deleted = 0 AND u.activo= 1');
+
+   	  return $r->execute();
+   }
+
+
    public static function getEmailEventoPublicar($IDaplic)
    {
    	 $r=Doctrine_Query::create()
