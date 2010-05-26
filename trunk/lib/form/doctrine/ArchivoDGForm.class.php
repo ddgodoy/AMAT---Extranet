f2b374b48095005898d61dbab16d8c3f9b814c75
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-05-26 15:28:10 +0000

    Mauro: Error en archivos de grupo de trabajo

--- trunk/lib/form/doctrine/ArchivoDGForm.class.php
+++ trunk/lib/form/doctrine/ArchivoDGForm.class.php
@@ -27,7 +27,6 @@ class ArchivoDGForm extends BaseArchivoDGForm
 			'grupo_trabajo_id'  => new sfWidgetFormChoice(array('choices' => array('0'=>'--seleccionar--')+$GruposUsuario), array('class' => 'form_input', 'style' => 'width: 200px;')),
 			'documentacion_grupo_id'  => new sfWidgetFormDoctrineChoice(array('model' => 'DocumentacionGrupo', 'add_empty' => false), array('class' => 'form_input', 'style' => 'width: 200px;')),
 			'contenido'         => new fckFormWidget(),			
-			'archivo'           => new sfWidgetFormInputFileEditable(array('file_src' => 'uploads/archivos_d_g/docs', 'template'  => '<div><label></label>%input%<br /><label></label>%delete%<label> Eliminar documento actual</label></div>', ), array('class' => 'form_input')),
 			'fecha'             => new sfWidgetFormJQueryDate(array('image'=>'/images/calendario.gif', 'format' => '%day%/%month%/%year%')),
 			'fecha_caducidad'   => new sfWidgetFormJQueryDate(array('image'=>'/images/calendario.gif', 'format' => '%day%/%month%/%year%')),
 			'disponibilidad'    => new sfWidgetFormChoice(array('choices' => array('Solo Grupo' => 'solo grupo', 'Todos' => 'todos'))),
@@ -38,7 +37,6 @@ class ArchivoDGForm extends BaseArchivoDGForm
 			'id'                => new sfValidatorDoctrineChoice(array('model' => 'ArchivoDG', 'column' => 'id', 'required' => false), array('required' => 'id req', 'invalid' => 'id inval' )),
 			'nombre'            => new sfValidatorString(array('required' => true), array('required' => 'El título es obligatorio')),
 			'contenido'         => new sfValidatorString(array('required' => false), array('required' => 'La descripción es obligatoria')),
-			'archivo'           => new sfValidatorFile(array('path' => 'uploads/archivos_d_g/docs', 'required' => false), $msArchivo),
 			'archivo_delete'    => new sfValidatorBoolean(),
 			'fecha'             => new sfValidatorDate(array('required' => true), array('required' => 'Debes seleccionar una fecha', 'invalid' => 'La fecha ingresada es incorrecta')),
 			'fecha_caducidad'   => new sfValidatorDate(array('required' => true), array('required' => 'Debes seleccionar una fecha de caducidad', 'invalid' => 'La fecha de caducidad ingresada es incorrecta')),
@@ -47,6 +45,20 @@ class ArchivoDGForm extends BaseArchivoDGForm
 			'documentacion_grupo_id' => new sfValidatorDoctrineChoice(array('model' => 'DocumentacionGrupo', 'required' => true),array('invalid' => 'La Documentacion es obligatoria')),
 			'owner_id'          => new sfValidatorDoctrineChoice(array('model' => 'Usuario', 'required' => true)),
 		));
+
+                if($this->getObject()->getArchivo())
+		{
+			$this->setWidget('archivo', new sfWidgetFormInputFileEditable(array('file_src' => 'uploads/archivos_d_g/docs', 'template'  => '<div><label></label>%input%<br /><label></label>%delete%<label> Eliminar documento actual</label></div>', ), array('class' => 'form_input')));
+			$this->setValidator('archivo', new sfValidatorFile(array('path' => 'uploads/archivos_d_g/docs', 'required' => false)));
+		    $this->setValidator('archivo_delete', new sfValidatorBoolean());
+		}
+		else
+		{
+
+		$this->setWidget('archivo', new sfWidgetFormInputFileEditable(array('file_src' => 'uploads/archivos_d_g/docs', 'template'  => '<div><label></label>%input%<br /><label></label></div>', ), array('class' => 'form_input')));
+		$this->setValidator('archivo', new sfValidatorFile(array('path' => 'uploads/archivos_d_g/docs', 'required' => false),array('required'=>'El archivo es obligatorio')));
+
+		}
 
 		$this->setDefaults(array(
 			'owner_id' => $userId,					
