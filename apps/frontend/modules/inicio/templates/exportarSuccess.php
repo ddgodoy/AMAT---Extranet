2e535a18ad0a984b041d5c1f92db914952fc5981
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-05-27 16:51:09 +0000

    Mauro: Probar el apartado Grupos de trabajo

--- apps/frontend/modules/inicio/templates/exportarSuccess.php
+++ apps/frontend/modules/inicio/templates/exportarSuccess.php
@@ -31,76 +31,76 @@ $f = 0;
 		 	   	  $r = utf8_decode($di);
 		 	   	  if ($hy == 'owner_id' || $hy == 'user_id_creador' ) 
 		 	   	  {
-		 	   	    echo '<td>'.htmlspecialchars_decode(trim(Usuario::datosUsuario($r))).'</td>';
+		 	   	    echo '<td>'.utf8_encode(htmlspecialchars_decode(trim(Usuario::datosUsuario($r)))).'</td>';
 		 	   	  }
 		 	   	  if($hy == 'mutua_id')
 		 	   	  {
 		 	   	  	if($r>= 1)
 		 	   	  	{
-		 	   	  	 echo '<td>'.htmlspecialchars_decode($reS->Mutua->getNombre()).'</td>';
+		 	   	  	 echo '<td>'.utf8_encode(htmlspecialchars_decode($reS->Mutua->getNombre())).'</td>';
 		 	   	  	}
 		 	   	  	echo '<td></td>';
 		 	   	  	 
 		 	   	  }
 		 	   	  if($hy == 'seccion_id')
 		 	   	  {
-		 	   	  	echo '<td>'.htmlspecialchars_decode(trim(CifraDatoSeccionTable::getIdseccion($r))).'</td>';
+		 	   	  	echo '<td>'.utf8_encode(htmlspecialchars_decode(trim(CifraDatoSeccionTable::getIdseccion($r)))).'</td>';
 		 	   	  }
 		 	   	  if($hy == 'circular_cat_tema_id')
 		 	   	  {
-		 	   	  	echo '<td>'.htmlspecialchars_decode(trim(CircularCatTemaTable::getCircularCat($r))).'</td>';
+		 	   	  	echo '<td>'.utf8_encode(htmlspecialchars_decode(trim(CircularCatTemaTable::getCircularCat($r)))).'</td>';
 		 	   	  }
 		 	   	  if($hy == 'grupo_trabajo_id')
 		 	   	  {
-		 	   	  	echo '<td>'.htmlspecialchars_decode(trim(GrupoTrabajoTable::getGrupoTrabajo($r))).'</td>';
+		 	   	  	echo '<td>'.utf8_encode(htmlspecialchars_decode(trim(GrupoTrabajoTable::getGrupoTrabajo($r)))).'</td>';
 		 	   	  }
 		 	   	   if($hy == 'categoria_d_g_id')
 		 	   	  {
-		 	   	  	echo '<td>'.htmlspecialchars_decode(trim(CategoriaDGTable::getCategoriaDG($r))).'</td>';
+		 	   	  	echo '<td>'.utf8_encode(htmlspecialchars_decode(trim(CategoriaDGTable::getCategoriaDG($r)))).'</td>';
 		 	   	  }
 		 	   	   if($hy == 'documentacion_grupo_id')
 		 	   	  {
-		 	   	  	echo '<td>'.htmlspecialchars_decode(trim(DocumentacionGrupoTable::getDocumentacionGrupoTrabajo($r))).'</td>';
+		 	   	  	echo '<td>'.utf8_encode(htmlspecialchars_decode(trim(DocumentacionGrupoTable::getDocumentacionGrupoTrabajo($r)))).'</td>';
 		 	   	  }
 		 	   	   if($hy == 'consejo_territorial_id')
 		 	   	  {
-		 	   	  	echo '<td>'.htmlspecialchars_decode(trim(ConsejoTerritorialTable::getConsejo($r))).'</td>';
+		 	   	  	echo '<td>'.utf8_encode(htmlspecialchars_decode(trim(ConsejoTerritorialTable::getConsejo($r)))).'</td>';
 		 	   	  }
 		 	   	   if($hy == 'categoria_c_t_id')
 		 	   	  {
-		 	   	  	echo '<td>'.htmlspecialchars_decode(trim(CategoriaCTTable::getCategoriasCT($r))).'</td>';
+		 	   	  	echo '<td>'.utf8_encode(htmlspecialchars_decode(trim(CategoriaCTTable::getCategoriasCT($r)))).'</td>';
 		 	   	  }
 		 	   	   if($hy == 'documentacion_consejo_id')
 		 	   	  {
-		 	   	  	echo '<td>'.htmlspecialchars_decode(trim(DocumentacionConsejoTable::getDocumentacionConsejo($r))).'</td>';
+		 	   	  	echo '<td>'.utf8_encode(htmlspecialchars_decode(trim(DocumentacionConsejoTable::getDocumentacionConsejo($r)))).'</td>';
 		 	   	  }
 		 	   	   if($hy == 'aplicacion_id')
 		 	   	  {
-		 	   	  	echo '<td>'.htmlspecialchars_decode(trim(AplicacionRolTable::getAplicacionRol($r))).'</td>';
+		 	   	  	echo '<td>'.utf8_encode(htmlspecialchars_decode(trim(AplicacionRolTable::getAplicacionRol($r)))).'</td>';
 		 	   	  }
 		 	   	  if($hy == 'rol_id')
 		 	   	  {
-		 	   	  	echo '<td>'.htmlspecialchars_decode(trim(RolTable::getRol($r))).'</td>';
+		 	   	  	echo '<td>'.utf8_encode(htmlspecialchars_decode(trim(RolTable::getRol($r)))).'</td>';
 		 	   	  }
 		 	   	  if($hy == 'categoria_organismo_id')
 		 	   	  {
-		 	   	  	echo '<td>'.htmlspecialchars_decode(trim(CategoriaOrganismoTable::getCategoriaOrganismo($r))).'</td>';
+		 	   	  	echo '<td>'.utf8_encode(htmlspecialchars_decode(trim(CategoriaOrganismoTable::getCategoriaOrganismo($r)))).'</td>';
 		 	   	  }
 		 	   	  if($hy == 'subcategoria_organismo_id')
 		 	   	  {
-		 	   	  	echo '<td>'.htmlspecialchars_decode(trim(SubCategoriaOrganismoTable::getSubcategoria($r))).'</td>';
+		 	   	  	echo '<td>'.utf8_encode(htmlspecialchars_decode(trim(SubCategoriaOrganismoTable::getSubcategoria($r)))).'</td>';
 		 	   	  }
 		 	   	  if($hy == 'organismo_id')
 		 	   	  {
-		 	   	  	echo '<td>'.htmlspecialchars_decode(trim(OrganismoTable::getOrganismo($r))).'</td>';
+		 	   	  	echo '<td>'.utf8_encode(htmlspecialchars_decode(trim(OrganismoTable::getOrganismo($r)))).'</td>';
 		 	   	  }
 		 	   	  if($hy == 'documentacion_organismo_id')
 		 	   	  {
-		 	   	  	echo '<td>'.htmlspecialchars_decode(trim(DocumentacionOrganismoTable::getDocumentacionOrganismo($r))).'</td>';
+		 	   	  	echo '<td>'.utf8_encode(htmlspecialchars_decode(trim(DocumentacionOrganismoTable::getDocumentacionOrganismo($r)))).'</td>';
 		 	   	  }
 		 	   	  if($hy != 'documentacion_organismo_id' && $hy != 'organismo_id' && $hy != 'subcategoria_organismo_id' && $hy != 'categoria_organismo_id' && $hy != 'rol_id' && $hy != 'aplicacion_id' && $hy != 'documentacion_consejo_id' && $hy != 'categoria_c_t_id'  &&  $hy != 'consejo_territorial_id' && $hy != 'documentacion_grupo_id' && $hy != 'categoria_d_g_id' &&  $hy != 'grupo_trabajo_id' && $hy!='salt' && $hy!='login' && $hy!='crypted_password' && $hy != 'owner_id' && $hy != 'user_id_creador' && $hy != 'mutua_id' && $hy != 'seccion_id' && $hy != 'circular_cat_tema_id' )
 		 	   	  {
-		 	        echo '<td>'.htmlspecialchars_decode(trim($r)).'</td>';
+		 	        echo '<td>'.utf8_encode(htmlspecialchars_decode(trim($r))).'</td>';
 		 	   	  }  
 		 	   	  
 		 	   }
@@ -157,7 +157,7 @@ $f = 0;
 		 	   	  {
 		 	   	  	echo '"'.CategoriaDGTable::getCategoriaDG($r).'",';
 		 	   	  }
-            if($hy == 'documentacion_grupo_id')
+                                   if($hy == 'documentacion_grupo_id')
 		 	   	  {
 		 	   	  	echo '"'.DocumentacionGrupoTable::getDocumentacionGrupoTrabajo($r).'",';
 		 	   	  }
