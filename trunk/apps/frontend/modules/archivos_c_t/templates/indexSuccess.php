0702e48f435e2e5afad9d67807fdc5829ced4335
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-05-28 17:48:01 +0000

    Mauro: Grupo de trabajo y documentacion por defecto al crear archivo, [OZK-433580] Error de navegacion en GT

--- trunk/apps/frontend/modules/archivos_c_t/templates/indexSuccess.php
+++ trunk/apps/frontend/modules/archivos_c_t/templates/indexSuccess.php
@@ -12,6 +12,11 @@ else
 		$arrayDocumentacion = DocumentacionConsejoTable::getAlldocumentacionC();
 	}
 ?>
+<?php if($grupoBsq && $documentacionBsq):
+$redireccionGrupo = 'archivo_c_t[documentacion_consejo_id]='.$documentacionBsq.'&consejo_territorial_id='.$grupoBsq;
+else :
+$redireccionGrupo = '';
+endif; ?>
 <link type="text/css" rel="stylesheet" href="/js/calendario/dhtml_calendar.css" media="screen"></link>
 <script language="javascript" type="text/javascript" src="/js/calendario/dhtml_calendar.js"></script>
 <script language="javascript" type="text/javascript" src="/js/common_functions.js"></script>
@@ -38,22 +43,15 @@ else
 	<div class="leftside">
 		<div class="lineaListados">
 			<?php if($pager->haveToPaginate()): ?>
-				<div style="float:left;" class="paginado"><?php echo test_pager($pager, $orderBy, $sortType) ?></div>
+				<div style="float:left;" class="paginado"><?php echo test_pager($pager, $orderBy, $sortType,$redireccionGrupoEdit) ?></div>
 			<?php endif; ?>
-                        <?php if($grupoBsq && $documentacionBsq):
-                              $redireccionGrupo = '?archivo_c_t[documentacion_consejo_id]='.$documentacionBsq.'&consejo_territorial_id='.$grupoBsq;
-                              $redireccionGrupoEdit = '&archivo_c_t[documentacion_consejo_id]='.$documentacionBsq.'&consejo_territorial_id='.$grupoBsq;
-                              else :
-                              $redireccionGrupo = '';
-                              $redireccionGrupoEdit = '';
-                              endif; ?>
 			<span class="info" style="float: left;">Hay <?php echo $cantidadRegistros ?> Registro/s <?php if ($cajaBsq) echo " con la palabra '".$cajaBsq."'" ?> </span> 
 			<?php if(validate_action('alta')): ?>
-			<input type="button" onclick="javascript:location.href='<?php echo url_for('archivos_c_t/nueva'.$redireccionGrupo) ?>';" style="float: right;" value="Nuevo Archivo" name="newNews" class="boton"/>
+			<input type="button" onclick="javascript:location.href='<?php echo url_for('archivos_c_t/nueva?'.$redireccionGrupo) ?>';" style="float: right;" value="Nuevo Archivo" name="newNews" class="boton"/>
 			<?php endif;?>
 		</div>
 		<?php if ($cantidadRegistros > 0) : ?>
-		<form method="post" enctype="multipart/form-data" action="<?php echo url_for('archivos_c_t/delete'.$redireccionGrupo) ?>">
+		<form method="post" enctype="multipart/form-data" action="<?php echo url_for('archivos_c_t/delete?'.$redireccionGrupo) ?>">
 		<table width="100%" cellspacing="0" cellpadding="0" border="0" class="listados">
 			<tbody>
 				<tr>
@@ -61,16 +59,16 @@ else
 					<th width="5%">&nbsp;</th>
 					<?php endif;?>
 					<th width="10%" style="text-align:left;">
-						<a href="<?php echo url_for('archivos_c_t/index?sort=fecha&type='.$sortType.'&page='.$paginaActual.'&orden=1') ?>">Fecha</a>
+						<a href="<?php echo url_for('archivos_c_t/index?sort=fecha&type='.$sortType.'&page='.$paginaActual.'&orden=1&'.$redireccionGrupo) ?>">Fecha</a>
 					</th>
 					<th width="35%">
-						<a href="<?php echo url_for('archivos_c_t/index?sort=nombre&type='.$sortType.'&page='.$paginaActual.'&orden=1') ?>">Titulo</a>
+						<a href="<?php echo url_for('archivos_c_t/index?sort=nombre&type='.$sortType.'&page='.$paginaActual.'&orden=1&'.$redireccionGrupo) ?>">Titulo</a>
 					</th>
 					<th width="15%">
-						<a href="<?php echo url_for('archivos_c_t/index?sort=consejo_territorial_id&type='.$sortType.'&page='.$paginaActual.'&orden=1') ?>">Consejo territorial</a>
+						<a href="<?php echo url_for('archivos_c_t/index?sort=consejo_territorial_id&type='.$sortType.'&page='.$paginaActual.'&orden=1&'.$redireccionGrupo) ?>">Consejo territorial</a>
 					</th>
 					<th width="15%">
-						<a href="<?php echo url_for('archivos_c_t/index?sort=owner_id&type='.$sortType.'&page='.$paginaActual.'&orden=1') ?>">Creado por</a>
+						<a href="<?php echo url_for('archivos_c_t/index?sort=owner_id&type='.$sortType.'&page='.$paginaActual.'&orden=1&'.$redireccionGrupo) ?>">Creado por</a>
 					</th>
 					<th width="5%">&nbsp;</th>
 					<th width="5%">&nbsp;</th>
@@ -85,7 +83,7 @@ else
 					</td>
 					<td valign="center">
 					<?php if(validate_action('listar')):?>
-						<a href="<?php echo url_for('archivos_c_t/show?id=' . $valor->getId().$redireccionGrupoEdit) ?>">
+						<a href="<?php echo url_for('archivos_c_t/show?id=' . $valor->getId().'&'.$redireccionGrupo) ?>">
 							<strong><?php echo $valor->getNombre() ?></strong>
 						</a>
 					<?php endif;?>	
@@ -102,14 +100,14 @@ else
 					</td>
 					<td valign="center" align="center">
 					<?php if(validate_action('modificar') || $valor->getOwnerId() == $sf_user->getAttribute('userId') ):?>
-						<a href="<?php echo url_for('archivos_c_t/editar?id=' . $valor->getId().$redireccionGrupoEdit) ?>">
+						<a href="<?php echo url_for('archivos_c_t/editar?id=' . $valor->getId().'&'.$redireccionGrupo) ?>">
 							<?php echo image_tag('show.png', array('height' => 20, 'width' => 17, 'border' => 0, 'title' => 'Ver')) ?>
 						</a>
 					<?php endif;?>	
 					</td>
                                           <td valign="center" align="center">
                                           <?php if(validate_action('baja') || $sf_user->getAttribute('userId') == $valor->getOwnerId()):?>
-                                                <?php echo link_to(image_tag('borrar.png', array('title'=>'Borrar','alt'=>'Borrar','width'=>'20','height'=>'20','border'=>'0')), 'archivos_c_t/delete?id='.$valor->getId().$redireccionGrupoEdit, array('method'=>'delete','confirm'=>'Confirma la eliminaci&oacute;n del registro?')) ?>
+                                                <?php echo link_to(image_tag('borrar.png', array('title'=>'Borrar','alt'=>'Borrar','width'=>'20','height'=>'20','border'=>'0')), 'archivos_c_t/delete?id='.$valor->getId().'&'.$redireccionGrupo, array('method'=>'delete','confirm'=>'Confirma la eliminaci&oacute;n del registro?')) ?>
                                           <?php endif;?>
                                           </td>
                                         </tr>
@@ -134,7 +132,7 @@ else
 		<?php if ($cantidadRegistros > 0) : ?>
 		<div class="lineaListados">
 			<?php if($pager->haveToPaginate()): ?>
-				<div style="float:left;" class="paginado"><?php echo test_pager($pager, $orderBy, $sortType) ?></div>
+				<div style="float:left;" class="paginado"><?php echo test_pager($pager, $orderBy, $sortType,$redireccionGrupo) ?></div>
 			<?php endif; ?>
 
 			<span class="info" style="float: left;">Hay <?php echo $cantidadRegistros ?> Registro/s</span>
@@ -152,7 +150,7 @@ else
 	<div class="rightside">
 		<div class="paneles">
 			<h1>Buscar</h1>
-			<form method="post" enctype="multipart/form-data" action="<?php echo url_for('archivos_c_t/index') ?>">
+			<form method="post" enctype="multipart/form-data" action="<?php echo url_for('archivos_c_t/index?'.$redireccionGrupo) ?>">
 				<table width="100%" cellspacing="4" cellpadding="0" border="0">
 				<tbody>
 					<tr>
