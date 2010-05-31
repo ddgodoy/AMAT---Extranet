35db1367f5f0a8c0b1d402fa415c3a756d26c2bf
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-05-31 13:36:07 +0000

    Mauro:  Error estilos en la ficha de documentacion

--- apps/frontend/modules/documentacion_grupos/templates/showSuccess.php
+++ apps/frontend/modules/documentacion_grupos/templates/showSuccess.php
@@ -14,7 +14,7 @@
 	
 	<div class="noticias">	  
 	  <a  class="nottit"><?php echo  $documentacion_grupo->getNombre() ?></a><br />
-	  <?php echo nl2br($documentacion_grupo->getcontenido()) ?> 
+	  <?php echo $documentacion_grupo->getcontenido() ?> 
 	  <span class="notfecha">
 	  <?php
 		if(ArchivoDG::getRepository()->getAllByDocumentacion($documentacion_grupo->getId())->count() >= 1){ 
