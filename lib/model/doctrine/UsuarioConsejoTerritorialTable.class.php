242bdaaf84567491e9880a32ce25be11a6813bde
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-06-01 19:51:42 +0000

    Mauro:  Probar el apartado Consejo

--- lib/model/doctrine/UsuarioConsejoTerritorialTable.class.php
+++ lib/model/doctrine/UsuarioConsejoTerritorialTable.class.php
@@ -12,7 +12,7 @@ class UsuarioConsejoTerritorialTable extends Doctrine_Table
 		->leftJoin('uc.ConsejoTerritorial ct')
 		->where('uc.consejo_territorial_id  ='.$id)
                 ->andWhere('uc.deleted = 0')
-                ->andWhere('u.deleted = 0')
+                ->andWhere('u.deleted = 0 AND u.activo = 1')
                 ->andWhere('ct.deleted = 0');
 		
 		return $q->execute();
