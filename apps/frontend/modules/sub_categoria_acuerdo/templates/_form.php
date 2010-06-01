3c7e0ff835f76eb8ffa6566f94cfb62a84776370
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-06-01 14:21:22 +0000

    Mauro:  Cambiar el boton cancelar [IT'S DONE]

--- apps/frontend/modules/sub_categoria_acuerdo/templates/_form.php
+++ apps/frontend/modules/sub_categoria_acuerdo/templates/_form.php
@@ -56,6 +56,6 @@
     <?php if(validate_action('alta') || validate_action('modificar')):?>
       <input type="submit" id="boton_guardar" class="boton" value="Guardar" name="btn_action"/>
     <?php endif;?>
-      <input type="button" id="boton_cancel" class="boton" value="Cancelar" name="boton_cancel" onclick="document.location='<?php echo url_for('sub_categoria_acuerdo/index') ?>';"/>
+      <input type="button" id="boton_cancel" class="boton" value="Volver" name="boton_cancel" onclick="document.location='<?php echo url_for('sub_categoria_acuerdo/index') ?>';"/>
     </div>
  </form>
