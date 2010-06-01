89a1d8d63b8bbcbcdd00b491a7767ffaa6c7fa65
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-06-01 14:05:01 +0000

    Mauro:  Cambiar titulo "Iniciativa" por "Actividad Parlamentaria"

--- trunk/apps/frontend/modules/iniciativas/templates/nuevaSuccess.php
+++ trunk/apps/frontend/modules/iniciativas/templates/nuevaSuccess.php
@@ -1,12 +1,12 @@
 <div class="mapa">
 	<!--<strong><?php //echo __('Administraci&oacute;n') ?> </strong>&gt;-->
 	<strong>Administraci&oacute;n </strong>&gt; 
-	<a href="<?php echo url_for('iniciativas/index') ?>">Iniciativas Formativas</a> &gt; 
-	Crear Iniciativa Formativa
+	<a href="<?php echo url_for('iniciativas/index') ?>">Actividad Parlamentaria</a> &gt;
+	Crear Actividad Parlamentaria
 </div>
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
 	<tr>
-		<td width="95%"><h1>Nueva Iniciativa</h1></td>
+		<td width="95%"><h1>Nueva Actividad Parlamentaria</h1></td>
 		<td width="5%" align="right">
 			<a href="#">
 				<?php echo image_tag('pregunta.gif', array('alt' => 'Ayuda', 'id' => 'sprytrigger1', 'width' => '29', 'height' => '30', 'border' => '0')) ?>
