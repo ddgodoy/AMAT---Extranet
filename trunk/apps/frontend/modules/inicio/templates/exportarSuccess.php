ff77ad052a2c035957ff3aff805308e47a0a344a
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-06-09 18:05:12 +0000

    Mauro: [TFC-479301] Caracteres extraños al exportar en excel

--- trunk/apps/frontend/modules/inicio/templates/exportarSuccess.php
+++ trunk/apps/frontend/modules/inicio/templates/exportarSuccess.php
@@ -144,7 +144,7 @@ $f = 0;
 		 	   	  {
 		 	   	  	echo '<td>'.utf8_decode(htmlspecialchars_decode(trim(DocumentacionOrganismoTable::getDocumentacionOrganismo($r)))).'</td>';
 		 	   	  }
-		 	   	  if($hy != 'subcategoria_normativa_dos_id' && $hy != 'subcategoria_normativa_uno_id' && $hy != 'categoria_normativa_id' && $hy != 'user_id_publicado' && $hy != 'user_id_modificado' && $hy != 'documentacion_organismo_id' && $hy != 'organismo_id' && $hy != 'subcategoria_organismo_id' && $hy != 'categoria_organismo_id' && $hy != 'rol_id' && $hy != 'aplicacion_id' && $hy != 'documentacion_consejo_id' && $hy != 'categoria_c_t_id'  &&  $hy != 'consejo_territorial_id' && $hy != 'documentacion_grupo_id' && $hy != 'categoria_d_g_id' &&  $hy != 'grupo_trabajo_id' && $hy!='salt' && $hy!='login' && $hy!='crypted_password' && $hy != 'owner_id' && $hy != 'user_id_creador' && $hy != 'mutua_id' && $hy != 'seccion_id' && $hy != 'circular_cat_tema_id' )
+		 	   	  if($hy != 'subcategoria_iniciativa_id' &&  $hy != 'categoria_iniciativa_id' && $hy != 'subcategoria_normativa_dos_id' && $hy != 'subcategoria_normativa_uno_id' && $hy != 'categoria_normativa_id' && $hy != 'user_id_publicado' && $hy != 'user_id_modificado' && $hy != 'documentacion_organismo_id' && $hy != 'organismo_id' && $hy != 'subcategoria_organismo_id' && $hy != 'categoria_organismo_id' && $hy != 'rol_id' && $hy != 'aplicacion_id' && $hy != 'documentacion_consejo_id' && $hy != 'categoria_c_t_id'  &&  $hy != 'consejo_territorial_id' && $hy != 'documentacion_grupo_id' && $hy != 'categoria_d_g_id' &&  $hy != 'grupo_trabajo_id' && $hy!='salt' && $hy!='login' && $hy!='crypted_password' && $hy != 'owner_id' && $hy != 'user_id_creador' && $hy != 'mutua_id' && $hy != 'seccion_id' && $hy != 'circular_cat_tema_id' )
 		 	   	  {
                                      if($r){
 		 	             echo '<td>'.utf8_decode(htmlspecialchars_decode(trim($r))).'</td>';
@@ -216,6 +216,22 @@ $f = 0;
                                         echo '"",';
                                        }
 		 	   	  }
+                                   if($hy == 'categoria_iniciativa_id')
+		 	   	  {
+                                       if($r){
+		 	   	  	echo '"'.utf8_decode(htmlspecialchars_decode(trim($reS->getCategoriaIniciativa()->getNombre()))).'",';
+                                       }else{
+                                        echo '"",';
+                                       }
+		 	   	  }
+                                  if($hy == 'subcategoria_iniciativa_id')
+		 	   	  {
+                                       if($r){
+		 	   	  	echo '"'.utf8_decode(htmlspecialchars_decode(trim($reS->getSubCategoriaIniciativa()->getNombre()))).'",';
+                                       }else{
+                                        echo '"",';
+                                       }
+		 	   	  }
 		 	   	 if($hy == 'mutua_id')
 		 	   	  {
 		 	   	  	echo '"'.$reS->Mutua->getNombre().'",';
@@ -277,7 +293,7 @@ $f = 0;
 		 	   	  {
 		 	   	  	echo '"'.DocumentacionOrganismoTable::getDocumentacionOrganismo($r).'",';
 		 	   	  }
-		 	   	  if($hy != 'subcategoria_normativa_dos_id' && $hy != 'subcategoria_normativa_uno_id' && $hy != 'categoria_normativa_id' &&$hy != 'user_id_publicado' && $hy != 'user_id_modificado' && $hy != 'documentacion_organismo_id' && $hy != 'organismo_id' && $hy != 'subcategoria_organismo_id' && $hy != 'categoria_organismo_id' && $hy != 'rol_id' && $hy != 'aplicacion_id' && $hy != 'documentacion_consejo_id' && $hy != 'categoria_c_t_id'  &&  $hy != 'consejo_territorial_id' && $hy != 'documentacion_grupo_id' && $hy != 'categoria_d_g_id' &&  $hy != 'grupo_trabajo_id' && $hy != 'owner_id' && $hy != 'user_id_creador' && $hy != 'mutua_id' && $hy != 'seccion_id' && $hy!='login' && $hy!='crypted_password'&& $hy!='salt')
+		 	   	  if($hy != 'subcategoria_iniciativa_id' &&  $hy != 'categoria_iniciativa_id' && $hy != 'subcategoria_normativa_dos_id' && $hy != 'subcategoria_normativa_uno_id' && $hy != 'categoria_normativa_id' &&$hy != 'user_id_publicado' && $hy != 'user_id_modificado' && $hy != 'documentacion_organismo_id' && $hy != 'organismo_id' && $hy != 'subcategoria_organismo_id' && $hy != 'categoria_organismo_id' && $hy != 'rol_id' && $hy != 'aplicacion_id' && $hy != 'documentacion_consejo_id' && $hy != 'categoria_c_t_id'  &&  $hy != 'consejo_territorial_id' && $hy != 'documentacion_grupo_id' && $hy != 'categoria_d_g_id' &&  $hy != 'grupo_trabajo_id' && $hy != 'owner_id' && $hy != 'user_id_creador' && $hy != 'mutua_id' && $hy != 'seccion_id' && $hy!='login' && $hy!='crypted_password'&& $hy!='salt')
 		 	   	  {
 		 	        echo '"'.$r.'",';
 		 	   	  }  
