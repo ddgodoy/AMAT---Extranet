3890ff1e0b26dbf135ab6a293385428198ad169b
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-06-09 17:59:32 +0000

    Mauro: [TFC-479301] Caracteres extraños al exportar en excel

--- apps/frontend/modules/iniciativas/templates/indexSuccess.php
+++ apps/frontend/modules/iniciativas/templates/indexSuccess.php
@@ -45,10 +45,10 @@
 					<th width="3%">&nbsp;</th>
                                         <?php endif; ?>
 					<th width="10%" style="text-align:center;">
-						<a href="<?php echo url_for('iniciativas/index?sort=fecha&type='.$sortType.'&page='.$paginaActual.'&orden=1') ?>">Fecha</a>
+						<a href="<?php echo url_for('iniciativas/index?sort=i.fecha&type='.$sortType.'&page='.$paginaActual.'&orden=1') ?>">Fecha</a>
 					</th>
 					<th width="77%">
-						<a href="<?php echo url_for('iniciativas/index?sort=nombre&type='.$sortType.'&page='.$paginaActual.'&orden=1') ?>">Titulo</a>
+						<a href="<?php echo url_for('iniciativas/index?sort=i.nombre&type='.$sortType.'&page='.$paginaActual.'&orden=1') ?>">Titulo</a>
 					</th>
 					<th width="5%">&nbsp;</th>
 					<th width="5%">&nbsp;</th>
