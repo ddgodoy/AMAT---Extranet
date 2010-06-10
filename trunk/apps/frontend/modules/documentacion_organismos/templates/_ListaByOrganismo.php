81da96c5690ee88fb9c76c7fce1a076a45f7838e
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-06-10 18:45:26 +0000

    Mauro: [OEQ-367169] Error en subcomisiones

--- trunk/apps/frontend/modules/documentacion_organismos/templates/_ListaByOrganismo.php
+++ trunk/apps/frontend/modules/documentacion_organismos/templates/_ListaByOrganismo.php
@@ -32,7 +32,7 @@
 						<?php echo $usuario->getApellido().', '.$usuario->getNombre() ?>
 					</td>
 					<td valign="center" align="center">
-						<?php if (validate_action('listar','archivos_d_o')) {
+						<?php if (validate_action('listar','archivos_d_o') && $valor->getEstado() == 'publicado') {
 								echo link_to(image_tag('archivos.png', array('border' => 0, 'title' => ArchivoDO::getRepository()->getAllByDocumentacion($valor->getId())->count().' Archivo/s')), 'archivos_d_o/index?archivo_d_o[documentacion_organismo_id]='.$valor->getId().'&archivo_d_o[organismo_id]='.$valor->getOrganismoId().$redireccionArchivo, array('method' => 'post'));
 							}?>
 					</td>
