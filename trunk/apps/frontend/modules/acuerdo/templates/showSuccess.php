a5cac1488972708379e520d1ebe756fa6640f93d
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-06-10 16:27:41 +0000

    Mauro: Error en titulos en documentacion

--- trunk/apps/frontend/modules/acuerdo/templates/showSuccess.php
+++ trunk/apps/frontend/modules/acuerdo/templates/showSuccess.php
@@ -12,7 +12,7 @@
 	<div class="noticias">	  
 	  <span class="notfecha">Publicado el: <?php echo date("d/m/Y", strtotime($acuerdo->getFecha())) ?></span> <br />
           <?php /*<img src="<?php if ($acuerdo->getimagen()): ?>/uploads/acuerdos/images/<?php echo $acuerdo->getimagen() ?> <?php else: ?> /images/noimage.jpg <?php endif; ?>" class="notimg" alt="<?php echo  $acuerdo->getNombre() ?>" /> */?>
-	  <a href="#" class="nottit"><?php echo  $acuerdo->getnombre() ?></a><br />
+	  <a class="nottit"><?php echo  $acuerdo->getnombre() ?></a><br />
 	  <?php if($acuerdo->getCategoriaAcuerdoId()):?>
           <span class="notfecha">Categor&iacute;a: <?php echo CategoriaAcuerdo::getRepository()->findOneById($acuerdo->getCategoriaAcuerdoId())->getNombre()?></span> <br />
 	  <?php endif;?>
