0baa2442ce56126a43d1b575b741faef5b63ea0a
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-06-11 15:41:22 +0000

    Mauro:  [F] - [OZK-433580] Error de navegacion en GT [REVISAR]

--- apps/frontend/modules/documentacion_organismos/templates/nuevaSuccess.php
+++ apps/frontend/modules/documentacion_organismos/templates/nuevaSuccess.php
@@ -18,6 +18,14 @@ $organismosArray = array('0'=>'','1'=>'');
 if($sf_request->getParameter('documentacion_organismo[organismo_id]')){
 $organismosArray = Organismo::getUrlOrganismos($sf_request->getParameter('documentacion_organismo[organismo_id]'),1);
 }
+
+echo $sf_->getGetParameter('documentacion_organismo[organismo_id]');
+exit ();
+
+if($sf_request->getParameter('documentacion_organismo[organismo_id]')):
+$redireccionGrupo = Organismo::getUrlOrganismos($sf_request->getParameter('documentacion_organismo[organismo_id]'));
+else: $redireccionGrupo = '';
+endif;
 include_partial(
 		'form',
 		array
