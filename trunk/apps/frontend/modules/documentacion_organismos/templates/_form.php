1dc1326422504ae9899f5b01c931a0c639bc02ea
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-06-11 15:02:38 +0000

    Mauro:  [F] - [OZK-433580] Error de navegacion en GT [REVISAR]

--- trunk/apps/frontend/modules/documentacion_organismos/templates/_form.php
+++ trunk/apps/frontend/modules/documentacion_organismos/templates/_form.php
@@ -11,7 +11,7 @@
 <ul class="ok_list"><li><?php echo $sf_user->getFlash('notice') ?></li></ul>
 <?php endif; ?>
 <?php
-echo $sf_params->get('hola[chau]');
+echo $sf_params->get('hola%5Bchau%5D');
 exit ();
 
 if($sf_request->getParameter('documentacion_organismo[organismo_id]')):
