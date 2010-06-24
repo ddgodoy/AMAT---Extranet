e76d34ed5ae1f731fae06f33cc331a8c0ef4a8db
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-06-24 15:41:02 +0000

    Mauro:  Aplicaciones externas cambio de Funcion

--- trunk/apps/frontend/lib/helper/CifradoSimetricoHelper.php
+++ trunk/apps/frontend/lib/helper/CifradoSimetricoHelper.php
@@ -22,7 +22,7 @@ function decifrado($string){
     $key   = sfConfig::get('app_key_aplication');
 
    $result = '';
-   $string = utf8_decode($string);;
+   $string = utf8_decode($string);
    for($i=0; $i<strlen($string); $i++) {
       $char = substr($string, $i, 1);
       $keychar = substr($key, ($i % strlen($key))-1, 1);
