44b1115904a778c22f498b995301741f7b58ecd1
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-07-02 13:52:55 +0000

    Mauro:  [AVC-645706] Ajuste en normativas

--- trunk/apps/frontend/modules/normativas/templates/_subcategoriasn1.php
+++ trunk/apps/frontend/modules/normativas/templates/_subcategoriasn1.php
@@ -1,3 +1,3 @@
-<?php use_helper('Javascript', 'Object') ?>
-<?php echo $witSub['subcategoria_normativa_uno_id'];
-echo  observe_field('normativa_subcategoria_normativa_uno_id',array('update'=>'dos','url'=>'normativas/subcategoriasn2','with'=>"'id_subcategoria='+value",'script'   => true,))?>
+<?php use_helper('Javascript', 'Object');
+echo $witSub['subcategoria_normativa_uno_id'];
+echo  observe_field('normativa_subcategoria_normativa_uno_id',array('update'=>'dos','url'=>'normativas/subcategoriasn2','with'=>"'id_subcategoria='+value+'&width=".$width."'",'script'   => true,))?>
