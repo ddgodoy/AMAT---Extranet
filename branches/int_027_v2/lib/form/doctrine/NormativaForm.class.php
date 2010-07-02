643a277a0385706e0caf92001261d81a2ca859d5
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-07-02 16:57:41 +0000

    Mauro: actulizacion del branches

--- branches/int_027_v2/lib/form/doctrine/NormativaForm.class.php
+++ branches/int_027_v2/lib/form/doctrine/NormativaForm.class.php
@@ -50,25 +50,25 @@ class NormativaForm extends BaseNormativaForm
   	$this->setWidgets(array(
   	  'fecha'             => new sfWidgetFormJQueryDate(array('image'=>'/images/calendario.gif', 'format' => '%day%/%month%/%year%','years' => array_combine($years, $years))),
   	  'publicacion_boe'             => new sfWidgetFormJQueryDate(array('image'=>'/images/calendario.gif', 'format' => '%day%/%month%/%year%','years' => array_combine($years, $years))),
-      'nombre'            => new sfWidgetFormInput(array(), array('style'=>'width:330px;','class'=>'form_input')),
-      'contenido'         => new fckFormWidget(),
-      'documento'         => new sfWidgetFormInputFileEditable(array('file_src' => '/uploads/normativas/docs/'.$this->getObject()->getdocumento(), 'template'  => '<div>%input%<br /><label> <a href="%file%" class="nottit" target="_blank">%file%</a></label><br />%delete%<label> Eliminar documento actual</label></div>', ), array('class' => 'form_input')),
-      'categoria_normativa_id'        => new sfWidgetFormChoice(array('choices' => $categoria)),
-      'subcategoria_normativa_uno_id' => new sfWidgetFormChoice(array('choices' => $subcategoriaN1)),
-      'subcategoria_normativa_dos_id' => new sfWidgetFormChoice(array('choices' => $subcategoriaN2)),
-    ));
+          'nombre'            => new sfWidgetFormInput(array(), array('style'=>'width:330px;','class'=>'form_input')),
+          'contenido'         => new fckFormWidget(),
+          'documento'         => new sfWidgetFormInputFileEditable(array('file_src' => '/uploads/normativas/docs/'.$this->getObject()->getdocumento(), 'template'  => '<div>%input%<br /><label> <a href="%file%" class="nottit" target="_blank">%file%</a></label><br />%delete%<label> Eliminar documento actual</label></div>', ), array('class' => 'form_input')),
+          'categoria_normativa_id'        => new sfWidgetFormChoice(array('choices' => $categoria), array('style'=>'width: 300px;')),
+          'subcategoria_normativa_uno_id' => new sfWidgetFormChoice(array('choices' => $subcategoriaN1), array('style'=>'width: 300px;')),
+          'subcategoria_normativa_dos_id' => new sfWidgetFormChoice(array('choices' => $subcategoriaN2), array('style'=>'width: 300px;')),
+        ));
 
-    $this->setValidators(array(
-      'fecha'             => new sfValidatorDate(array(), array('required' => 'Debes seleccionar una fecha', 'invalid' => 'La fecha ingresada es incorrecta')),
-      'publicacion_boe'   => new sfValidatorDate(array('required' =>false), array('required' => 'Debes seleccionar una fecha', 'invalid' => 'La fecha ingresada es incorrecta')),
-      'nombre'            => new sfValidatorString(array('required' => true), array('required'=>'El Nombre es obligatorio')),
-      'contenido'         => new sfValidatorString(array('required' => true), array('required'=>'El Contenido es obligatorio')),
-      'documento'         => new sfValidatorFile(array('path' => 'uploads/normativas/docs','required' => false, 'mime_types'=>array('application/msword', 'application/pdf'),) , array('mime_types' => 'Formato de documento incorrecto, permitidos (.doc, .pdf )')),
-      'categoria_normativa_id'        => new sfValidatorChoice(array('choices' => array_keys($categoria), 'required' => false),array('invalid'=>'0')),
-      'subcategoria_normativa_uno_id' => new sfValidatorChoice(array('choices' => array_keys($subvalidcatN1), 'required' => false),array('invalid'=>'1')),
-      'subcategoria_normativa_dos_id' => new sfValidatorChoice(array('choices' => array_keys($subvalidcatN2), 'required' => false),array('invalid'=>'2')),
-      'documento_delete'  => new sfValidatorBoolean(),
-    ));
+        $this->setValidators(array(
+          'fecha'             => new sfValidatorDate(array(), array('required' => 'Debes seleccionar una fecha', 'invalid' => 'La fecha ingresada es incorrecta')),
+          'publicacion_boe'   => new sfValidatorDate(array('required' =>false), array('required' => 'Debes seleccionar una fecha', 'invalid' => 'La fecha ingresada es incorrecta')),
+          'nombre'            => new sfValidatorString(array('required' => true), array('required'=>'El Nombre es obligatorio')),
+          'contenido'         => new sfValidatorString(array('required' => true), array('required'=>'El Contenido es obligatorio')),
+          'documento'         => new sfValidatorFile(array('path' => 'uploads/normativas/docs','required' => false, 'mime_types'=>array('application/msword', 'application/pdf'),) , array('mime_types' => 'Formato de documento incorrecto, permitidos (.doc, .pdf )')),
+          'categoria_normativa_id'        => new sfValidatorChoice(array('choices' => array_keys($categoria), 'required' => false),array('invalid'=>'0')),
+          'subcategoria_normativa_uno_id' => new sfValidatorChoice(array('choices' => array_keys($subvalidcatN1), 'required' => false),array('invalid'=>'1')),
+          'subcategoria_normativa_dos_id' => new sfValidatorChoice(array('choices' => array_keys($subvalidcatN2), 'required' => false),array('invalid'=>'2')),
+          'documento_delete'  => new sfValidatorBoolean(),
+        ));
 
     $this->widgetSchema->setLabels(array(
     		'fecha'     => 'Fecha *',
