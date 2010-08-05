81d3694c4ad272c3b0700e62d40c8d27eb799c3c
mauro <mauro@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-08-05 19:20:02 +0000

    Mauro:  [WWM-830860] Duda funciona subgrupos

---
+++ apps/frontend/modules/miembros_organismo_lista/templates/_MenuOrganismos.php
@@ -0,0 +1,9 @@
+<h2 class="grupo"><?php if($organismo!=''): echo $organismo->getCategoriaOrganismo()->getNombre().' '.$organismo->getSubCategoriaOrganismo()->getNombre() .' '.$organismo->getNombre(); $get = '?organismo='.$organismo->getId(); else: $get= ''; echo 'Organismos'; endif; ?> </h2>
+       <div class="navegacion-grupos">
+       <!--<a <?php //if($modulo != 'normas_de_funcionamientos'):?> href="<?php //echo url_for('normas_de_funcionamientos/index'.$get) ?>" <?php // else: ?> class="seleccionado" <?php //endif; ?>>Normas de Funcionamiento</a> | -->
+       <?php if ( validate_action('listar','miembros_organismo_lista')): ?><a <?php if($modulo != 'miembros_organismo_lista'):?> href="<?php echo url_for('miembros_organismo_lista/index'.$get) ?>" <?php else: ?> class="seleccionado" <?php endif; ?>>Miembros de Organismos</a> | <?php endif;?>
+       <?php if ( validate_action('listar','asambleas_organismos_lista')): ?><a <?php if($modulo != 'asambleas_organismos_lista'):?> href="<?php echo url_for('asambleas_organismos_lista/index?grupodetrabajo=Organismo_'.$organismo->getId()) ?>" <?php else: ?> class="seleccionado" <?php endif; ?>>Convocatorias</a> | <?php endif;?>
+       <?php if ( validate_action('listar','acta_organismos_lista')): ?><a <?php if($modulo != 'acta_organismos_lista'):?> href="<?php echo url_for('acta_organismos_lista/index?grupodetrabajo=Organismo_'.$organismo->getId()).'&Organismo=4' ?>" <?php else: ?> class="seleccionado" <?php endif; ?>>Actas </a> | <?php endif;?>
+       <?php if($organismo!=''){$getDoc = Organismo::getUrlOrganismos($organismo->getId());}else{$getDoc = '';}?>
+       <?php if ( validate_action('listar','documentacion_organismos_lista')): ?><a <?php if($modulo != 'documentacion_organismos_lista'):?> href="<?php echo url_for('documentacion_organismos_lista/index?'.$getDoc); ?>" <?php else:?> class="seleccionado" <?php endif; ?>>Documentaci&oacute;n</a><?php endif;?>
+</div>
