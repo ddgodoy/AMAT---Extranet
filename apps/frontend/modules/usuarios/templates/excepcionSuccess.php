83ca3d0005800f4440dd81451b9fa9940daf0d4e
mauro2 <mauro2@c1cede6c-d957-40ee-9f88-d724de6feb4e>
2010-12-28 17:22:33 +0000

    Mauro: Añadir excepciones de permisos al editar un usuario

---
+++ apps/frontend/modules/usuarios/templates/excepcionSuccess.php
@@ -0,0 +1,13 @@
+<div class="mapa">
+	<strong>Administraci&oacute;n </strong>&gt; 
+        <a href="<?php echo url_for('usuarios/index') ?>">Gesti&oacute;n de Usuarios</a> &gt; Actualizar Usuario &gt; Nueva Excepci&oacute;n
+</div>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+	<tr>
+		<td width="95%"><h1>Actualizar Usuario</h1></td>
+		<td width="5%" align="right">
+			<a href="#"><?php echo image_tag('pregunta.gif', array('alt' => 'Ayuda', 'id' => 'sprytrigger1', 'width' => '29', 'height' => '30', 'border' => '0')) ?></a>
+		</td>
+	</tr>
+</table>
+
